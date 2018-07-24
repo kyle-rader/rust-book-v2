cb856c8b26affafe878ecd56b4dd67b5e339a23d
Kyle Rader <kyrader@microsoft.com>
2018-07-24 14:31:11 -0700

    Finish ch03 guessing game

--- ch03-guessing-game/src/main.rs
+++ ch03-guessing-game/src/main.rs
@@ -1,14 +1,39 @@
+extern crate rand;
+
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
-    println!("Hello Guessing Ganme!");
-    println!("Please input your guess.");
+    println!("Hello Guessing Game!");
 
-    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read input!");
+    loop {
+        let mut guess = String::new();
+        println!("Please input your guess.");
 
-    println!("Your guess was: {}", guess);
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read input!");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(err) => {
+                println!("Positive integers only please: {}", err);
+                continue;
+            },
+        };
+
+        println!("Your guess was: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("That's it! You win!");
+                break;
+            },
+        }
+    }
 }
