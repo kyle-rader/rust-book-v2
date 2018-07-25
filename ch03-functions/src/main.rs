47706642a1e338b01e781c64128587fc58b6cbe1
kyrader <kyrader@microsoft.com>
2018-07-25 08:49:21 -0700

    Add function with args

--- ch03-functions/src/main.rs
+++ ch03-functions/src/main.rs
@@ -2,8 +2,14 @@ fn main() {
     println!("Hello, Functions!");
 
     my_first_function();
+
+    println!("2 to the power of 8 is: {}", pow(2, 8));
 }
 
 fn my_first_function() {
     println!("I come from a function :)");
+}
+
+fn pow(base: i32, power: u32) -> i32 {
+    base.pow(power)
 }
